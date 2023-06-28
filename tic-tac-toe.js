08231a4915dd6a51269faec261408d21dcfbedac
tada <rortheford@gmail.com>
2023-06-27 20:36:32 -0700

    Add functionality for a turn and win conditions

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,60 +1,103 @@
+//gameboard
 const gameboard = (() => {
-    let rowCount = 3;
-    let colCount = 3;
+
+    //size of the board
+    let slotCount = 9;
     let board = [];
 
-    for (let i = 0; i < rowCount; i++)
+    //fill board with 0's
+    for (let i = 0; i < slotCount; i++)
     {
-        board.push([]);
-        for (let j = 0; j < colCount; j++)
-        {
-            board[i].push(0);
-        }
+        board[i] = i + 3;
     }
 
-    const move = (order, row, column) => {
-        if (board[row][column] !== 0) {
-            console.log("This position is already filled");
-            return -1;
-        }
-        else if (order === 1) {
-            board[row][column] = 1;
+    const move = (symbol, slot) => {
+        if (symbol === "X") {
+            board[slot] = 1;
+            console.log(board);
             return 0;
         }
-        else if (order === 2) {
-            board[row][column] = -1;
+        //set board slot to 2 for Circles
+        else if (symbol === "O") {
+            board[slot] = 2;
+            console.log(board);
             return 0;
         }
     }
 
-    return { move };
+    const checkWin = (count) => {
+        if (board[0] === board[1] && board[0] === board[2])
+            return 1;
+        else if (board[3] === board[4] && board[3] === board[5]) 
+            return 1;
+        else if (board[6] === board[7] && board[6] === board[8]) 
+            return 1;
+        else if (board[0] === board[3] && board[0] === board[6]) 
+            return 1;
+        else if (board[1] === board[4] && board[1] === board[7]) 
+            return 1;       
+        else if (board[2] === board[5] && board[2] === board[8]) 
+            return 1;
+        else if (board[0] === board[4] && board[0] === board[8]) 
+            return 1;   
+        else if (board[2] === board[4] && board[2] === board[6]) 
+            return 1;
+        else if (count === 9) 
+            return 2;
+        else 
+            return 0;
+    }
+
+    const hello = () => {
+        console.log("hello");
+    }
+
+    return { move, hello, checkWin };
 })();
 
+//game
 const game = (() => {
 
-    const player = (name, order) => {
-        return { name, order }
+    let counter = 0;
+
+    //player with name and symbol
+    const player = (name, symbol) => {
+        return { name, symbol }
     };
 
-    const player1 = player("Player One", 1);
-    const player2 = player("Player Two", 2);
+    const player1 = player("Player One", "X");
+    const player2 = player("Player Two", "O");
 
     const players = [player1, player2];
 
     let currentPlayer = players[0];
 
+    //switch using ternary operator
     const switchTurns = () => {
         currentPlayer = currentPlayer === player1 ? player2 : player1;
     }
 
-    const turn = () => {
-        gameboard.display();
-        console.log(`${currentPlayer.name}'s turn`);
-        let row = prompt("What row to play?");
-        let column = prompt("What column to play?");
+    let buttons = document.querySelectorAll(".board_slot");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            turn(button.id);
+        });
+    });
 
-        if (gameboard.move(currentPlayer.order, row, column) !== -1)
-            switchTurns();
+    //one turn of a player
+    const turn = (id) => {
+        gameboard.move(currentPlayer.symbol, parseInt(id));
+        let current = document.getElementById(id);
+        current.setAttribute("disabled", true);
+        current.textContent = currentPlayer.symbol;
+        counter++;
+        let result = gameboard.checkWin(counter);
+        console.log(result);
+        if (result === 1)
+            alert(`${currentPlayer.name} Wins!`);
+        else if (result === 2)  
+            alert("It's a Tie!");
+        switchTurns();
     };
 })();
 
