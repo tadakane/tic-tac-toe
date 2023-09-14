7ad13ca5dc1dba7521fef8e1ace056d192762f36
tada <rortheford@gmail.com>
2023-09-13 23:55:10 -0700

    Add result messages and play again button

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,7 +5,7 @@ const gameboard = (() => {
     let slotCount = 9;
     let board = [];
 
-    //fill board with 0's
+    //fill board with different numbers from 3 to 11 to use later to check if there is a 3 in a row on the board
     for (let i = 0; i < slotCount; i++)
     {
         board[i] = i + 3;
@@ -14,17 +14,16 @@ const gameboard = (() => {
     const move = (symbol, slot) => {
         if (symbol === "X") {
             board[slot] = 1;
-            console.log(board);
             return 0;
         }
         //set board slot to 2 for Circles
         else if (symbol === "O") {
             board[slot] = 2;
-            console.log(board);
             return 0;
         }
     }
 
+    //check for all winning conditions
     const checkWin = (count) => {
         if (board[0] === board[1] && board[0] === board[2])
             return 1;
@@ -42,17 +41,31 @@ const gameboard = (() => {
             return 1;   
         else if (board[2] === board[4] && board[2] === board[6]) 
             return 1;
+        //or a tie
         else if (count === 9) 
             return 2;
+        //or no result yet
         else 
             return 0;
     }
 
-    const hello = () => {
-        console.log("hello");
+    //reset board array, visual gameboard, and message boxes
+    const resetBoard = () => {
+        for (let i = 0; i < slotCount; i++) {
+            board[i] = i + 3;
+        }
+        let buttons = document.querySelectorAll(".board_slot");
+        buttons.forEach((button) => {
+            button.textContent = "";
+            button.disabled = false;
+        });
+        let resultBox = document.querySelector(".result");
+        let againBox = document.querySelector(".again");
+        resultBox.removeChild(resultBox.firstChild);
+        againBox.removeChild(againBox.firstChild);
     }
 
-    return { move, hello, checkWin };
+    return { move, checkWin, resetBoard };
 })();
 
 //game
@@ -84,19 +97,68 @@ const game = (() => {
         });
     });
 
+    const displayWinner = (result) => {
+        let resultBox, resultMessage, againBox, againBtn;
+        //if a result is decided
+        if (result !== 0) {
+            //add the messages for result and playing again
+            resultBox = document.querySelector(".result");
+            resultMessage = document.createElement("div");
+            againBox = document.querySelector(".again");
+            againBtn = document.createElement("button");
+            resultMessage.style.color = againBtn.style.color = 'white';
+            resultMessage.style.fontFamily = againBtn.style.fontFamily = 'Amatic';
+            resultMessage.style.fontSize = againBtn.style.fontSize = '100px';
+            againBtn.style.backgroundColor = 'black';
+            againBtn.style.border = 'none';
+            againBtn.setAttribute('id', 'againBtn');
+            // againBtn.addEventListener("mouseover", (event) => {
+            //     againBtn.style.color = "yellow";
+            // });
+            //change text size if hovered, added play againr reset function to button
+            againBtn.onmouseenter = () => againBtn.style.fontSize = "120px";
+            againBtn.onmouseleave = () => againBtn.style.fontSize = "100px";
+            againBtn.onclick = () => gameboard.resetBoard();
+        }
+        //in the event a player won
+        if (result === 1) {
+            //display winner, display buttons, prompt to play again, reset counter and player
+            resultMessage.textContent = `${currentPlayer.name} Wins!`;
+            resultBox.appendChild(resultMessage);
+            buttons.forEach((button) => {
+                button.disabled = true;
+            })
+            againBtn.textContent = "Play Again?";
+            againBox.appendChild(againBtn);
+            counter = 0;
+            currentPlayer = player2;
+        }
+        //in the event of a tie
+        else if (result === 2) {
+            resultMessage.textContent = "It's a Tie!";
+            resultBox.appendChild(resultMessage);
+            againBtn.textContent = "Play Again?";
+            againBox.appendChild(againBtn);
+            counter = 0;
+            currentPlayer = player2;
+        }
+    }
+
     //one turn of a player
     const turn = (id) => {
+        //pass in id of button, call move function and update board accordint to current player
         gameboard.move(currentPlayer.symbol, parseInt(id));
         let current = document.getElementById(id);
+        //disable that button
         current.setAttribute("disabled", true);
         current.textContent = currentPlayer.symbol;
+        //update counter for tie check
         counter++;
+        //check for win
         let result = gameboard.checkWin(counter);
-        console.log(result);
-        if (result === 1)
-            alert(`${currentPlayer.name} Wins!`);
-        else if (result === 2)  
-            alert("It's a Tie!");
+        //call display winner
+        displayWinner(result);
+        //switch turn
         switchTurns();
     };
 })();
